cf4191287c9a80333c83e08dc68902abfbfe4977
Alka sah <alkasah485@gmail.com>
2022-07-01 17:54:40 -0700

    modification saved

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,25 @@
 console.log("hello");
 var today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do"));
+//Declare current time
+var currentTime = moment().format("H");
+console.log(currentTime);
+var timeBlockEl = $("#timeBlock");
+console.log(timeBlockEl);
+for (var i =9 ; i<=17;i++){
+
+     var key ="hour-"+i;
+     console.log(key);
+     if(i>currentTime)
+     {
+        console.log("future time");
+     }
+     else if(i<currentTime){
+        console.log("past time");
+     }
+     else{
+        console.log("current time");
+     }
+     
+
+}
