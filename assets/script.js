8dd8a74743f8b142ac68b8111258de5c3c9dc005
Alka sah <alkasah485@gmail.com>
2022-07-02 08:59:20 -0700

    modification in HTML file

--- assets/script.js
+++ assets/script.js
@@ -10,24 +10,32 @@ for (var i =9 ; i<=17;i++){
 
      var key ="hour-"+i;
      console.log(key);
+     var template = 
+    $('<div class = "row"><div class ="hour">'+i+'AM</div><textarea cols="100"></textarea><div><button data-hour='+i+' class="saveBtn">Save</button></div></div>');
      if(i>currentTime)
      {
+        timeBlockEl.addClass('future')
+        //template.addClass('future');
         console.log("future time");
      }
      else if(i<currentTime){
+        
+        timeBlockEl.addClass('past');
+        //template.addClass('past');
         console.log("past time");
      }
      else{
+        //template.addClass('present');
+        timeBlockEl.addClass('present');
         console.log("current time");
      }
-     var data = "sdf"
-     
-     var template = 
-      $('<div class = "row"><div class ="hour">'+i+'AM</div><textarea cols="100"></textarea><div><button data-hour='+i+' class="saveBtn">Save</button></div></div>');
+    var data = "sdf"
+    var template = 
+    $('<div class = "row"><div class ="hour">'+i+'AM</div><textarea cols="100"></textarea><div><button data-hour='+i+' class="saveBtn">Save</button></div></div>');
       console.log (template);
    
       //console.log (template);
-      timeBlockEl.append(template);
+    timeBlockEl.append(template);
       
      
 
