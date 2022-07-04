abb9c7466367d6ac154b510c9206c8a6f0c8cccc
Alka sah <alkasah485@gmail.com>
2022-07-04 02:36:53 -0700

    modification saved

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,16 @@ var today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do"));
 //Declare current time
 var currentTime = moment().format("H");
-console.log(currentTime);
 var timeBlockEl = $("#timeBlock");
-console.log(timeBlockEl);
-for (var i =9 ; i<=17;i++){
 
+for (var i =9 ; i<=17;i++){
      var key ="hour-"+i;
      console.log(key);
      var template = 
     $('<div class = "row"><div class ="hour w-25 p-4 col-1">'+i+'AM</div><textarea class ="description w-50 p-4 col-10"></textarea><button data-hour='+i+' class="saveBtn w-25 p-2 col-1 fas fa-save fa-2x"></button></div><p></p>');
-     
     var data = "sdf"
-    
       console.log (template);
     timeBlockEl.append(template);
-
     if(i>currentTime){
         console.log("future time");
         template.removeClass("past");
@@ -34,9 +29,20 @@ for (var i =9 ; i<=17;i++){
         template.addClass("present");
     }      
 }
+var printEvent = function(descr){
+
+
+
+}
 
 $(".saveBtn").on("click" , function(){
     console.log("save");
-
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    console.log(text);
+    //var time = $(this).parent().attr("hour");
+    var time = $(this).siblings(".hour").text();
+    console.log(time);
+    localStorage.setItem(time,text);
 });
     
